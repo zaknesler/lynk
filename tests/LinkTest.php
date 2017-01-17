71361ab6334053a2edf6456b55bb2510c5563448
Zak Nesler <zaknesler@gmail.com>
2017-01-16 22:43:53 -0500

    Added more tests

--- tests/LinkTest.php
+++ tests/LinkTest.php
@@ -7,7 +7,7 @@ use Illuminate\Foundation\Testing\DatabaseTransactions;
 
 class LinkTest extends TestCase
 {
-    use DatabaseTransactions;
+    use DatabaseMigrations;
 
     /** @test */
     public function it_creates_a_link_with_a_randomly_generated_code()
@@ -73,7 +73,6 @@ class LinkTest extends TestCase
         ]);
 
         $this->seeStatusCode(200);
-
         $this->assertEquals(1, Link::count());
     }
 
@@ -93,7 +92,41 @@ class LinkTest extends TestCase
         ]);
 
         $this->seeStatusCode(422);
-
         $this->assertEquals(1, Link::count());
+    }
+
+    /** @test */
+    function it_does_not_create_a_link_if_the_url_is_invalid_and_there_is_a_custom_code()
+    {
+        $this->json('POST', '/links', [
+            'url' => 'not-a-valid-url',
+            'code' => 'example',
+        ]);
+
+        $this->seeStatusCode(422);
+        $this->assertEquals(0, Link::count());
+    }
+
+    /** @test */
+    function it_does_not_create_a_link_if_the_url_is_invalid_and_there_is_no_custom_code()
+    {
+        $this->json('POST', '/links', [
+            'url' => 'not-a-valid-url',
+        ]);
+
+        $this->seeStatusCode(422);
+        $this->assertEquals(0, Link::count());
+    }
+
+    /** @test */
+    function it_does_not_create_a_link_if_the_code_is_too_short()
+    {
+        $this->json('POST', '/links', [
+            'url' => 'https://google.com',
+            'code' => 'a',
+        ]);
+
+        $this->seeStatusCode(422);
+        $this->assertEquals(0, Link::count());
     }
 }
