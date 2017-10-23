3a6bc128323e60cdea532d0f1612f39d02a7b76a
Zak Nesler <zaknesler@gmail.com>
2017-10-23 09:01:06 -0400

    Add meta tags

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -4,9 +4,12 @@
         <meta charset="UTF-8" />
         <meta http-equiv="X-UA-Compatible" content="IE=edge" />
         <meta name="viewport" content="width=device-width, user-scalable=no, initial-scale=1.0, maximum-scale=1.0, minimum-scale=1.0" />
+        <meta name="Description" content="Shorten urls easily.">
+        <meta name="Keywords" content="url shortener, url, shorten, lynk">
 
         <title>{{ config('app.name') }}</title>
 
+        <link rel="icon" href="/favicon.jpg" />
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:400,700" />
         <link rel="stylesheet" href="{{ mix('/css/app.css') }}" />
 
