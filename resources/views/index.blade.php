ddc09655d7ab2d7b50975b136b71cc4f230047bf
Zak Nesler <zaknesler@gmail.com>
2017-10-25 09:02:41 -0400

    Add google analytics

--- resources/views/index.blade.php
+++ resources/views/index.blade.php
@@ -13,7 +13,9 @@
         <link rel="stylesheet" href="https://fonts.googleapis.com/css?family=Lato:400,700" />
         <link rel="stylesheet" href="{{ mix('/css/app.css') }}" />
 
+        <script async src="https://www.googletagmanager.com/gtag/js?id=UA-78906254-4"></script>
         <script>window.Lynk = {!! json_encode(['csrfToken' => csrf_token()]) !!};</script>
+        <script>window.dataLayer=window.dataLayer||[];function gtag(){dataLayer.push(arguments);}gtag('js',new Date());gtag('config','UA-78906254-4');</script>
     </head>
     <body>
         <div class="container">
